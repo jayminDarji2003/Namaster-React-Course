456f67e13064b442e4ec741327e6dbaf609ad23d
Jaymin Darji <jaymindarji2003@gmail.com>
2023-07-28 19:45:53 +0530

    EP done

---
+++ EP_1_Inception/App.js
@@ -0,0 +1,30 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title1",
+    className: "title11",
+  },
+  "heading 1"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title2",
+    className: "title22",
+  },
+  "heading 2"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// passing a react element inside the root (root => id => container)
+root.render(container);
