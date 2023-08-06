bb275d9cbd2e8ada25b333d7521386c6fa0d7ff1
Jaymin Darji <jaymindarji2003@gmail.com>
2023-08-06 14:56:30 +0530

    just do some code

--- FoodVilla Application/foodvilla/src/components/Card.js
+++ FoodVilla Application/foodvilla/src/components/Card.js
@@ -1,21 +1,26 @@
 import React from 'react'
 
+// Config Driven UI
 
 const burgerKing = {
-    name : "Burger King",
-    imgUrl : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf",
-
+    name: "Burger King",
+    imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    cusines: ["Burger", "American"],
+    rating: "4.5"
 }
+
 
 function Card() {
     return (
         <div className="card">
             <img src={burgerKing.imgUrl} alt="Image" />
             <h3>{burgerKing.name}</h3>
-            <p>Burgers, American</p>
-            <p>4.5 Stars</p>
+            <p>{burgerKing.cusines.join(", ")}</p>
+            <p><span className="rating">{burgerKing.rating}</span> starts</p>
         </div>
     )
 }
 
 export default Card
+
+
