fd6ac01654f80c0f357807477523fba4028f2690
Jaymin Darji <jaymindarji2003@gmail.com>
2023-08-07 10:57:13 +0530

    Created API of Swiggy

--- FoodVilla Application/foodvilla/src/components/Card.js
+++ FoodVilla Application/foodvilla/src/components/Card.js
@@ -1,22 +1,12 @@
 import React from 'react'
 
-// Config Driven UI
-
-const burgerKing = {
-    name: "Burger King",
-    imgUrl: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf",
-    cusines: ["Burger", "American"],
-    rating: "4.5"
-}
-
-
 function Card() {
     return (
         <div className="card">
-            <img src={burgerKing.imgUrl} alt="Image" />
-            <h3>{burgerKing.name}</h3>
-            <p>{burgerKing.cusines.join(", ")}</p>
-            <p><span className="rating">{burgerKing.rating}</span> starts</p>
+            {/* <img src={} alt="Image" /> */}
+            {/* <h3>{}</h3>
+            <p>{}</p>
+            <p><span className="rating">{}</span> starts</p> */}
         </div>
     )
 }
