0305e904a57af0f006b920f9d42b5fa03c659f62
Jaymin Darji <jaymindarji2003@gmail.com>
2023-08-11 21:04:47 +0530

    learned useState Hook today and create search component

--- FoodVilla Application/src/components/Search.js
+++ FoodVilla Application/src/components/Search.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Search() {
+    const [searchTxt, setSearchTxt] = useState("");
+    // we are creating a one state where the initial value is false and if the user clicked on search then do true to variable
+    const [isClicked, setIsClicked] = useState("false");
+
     return (
-        <div className='search-container'>
-            <form>
-                <input type="text" placeholder='Search Any Restaurant' />
-            </form>
-                <button>Search</button>
-        </div>
+        <>
+            <div className='search-container'>
+                <form>
+                    <input
+                        type="text"
+                        className='search-input'
+                        placeholder='Search Any Restaurant'
+                        value={searchTxt}
+                        onChange={(e) => {
+                            // e.target.value --> input value
+                            setSearchTxt(e.target.value);
+                        }}
+                    />
+                </form>
+
+                <button onClick={() => {
+                    setIsClicked("true");
+                }}>
+                    Search
+                </button>
+            </div>
+        </>
+
     )
 }
 
