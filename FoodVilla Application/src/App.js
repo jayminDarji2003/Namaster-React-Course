a1a9bae9357316222758d6c567338671d77319af
Jaymin Darji <jaymindarji2003@gmail.com>
2023-08-23 19:03:36 +0530

    I'm back after a week

--- FoodVilla Application/src/App.js
+++ FoodVilla Application/src/App.js
@@ -5,8 +5,8 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header/>
-      <Body/>
+      <Header />
+      <Body />
     </>
   )
 }
--- FoodVilla Application/src/components/Body.js
+++ FoodVilla Application/src/components/Body.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 import Card from './Card';
 import { restaurantList } from '../config';
@@ -6,7 +6,7 @@ import { restaurantList } from '../config';
 
 function filterData(searchTxt, restaurants) {
   const filterRes = restaurants.filter((restaurant) => {
-    return restaurant.data.name.includes(searchTxt);
+    return restaurant.data.name.toLowerCase().includes(searchTxt.toLowerCase());
   });
 
   return filterRes;
@@ -17,8 +17,24 @@ function Body() {
   const [restaurants, setRestaurants] = useState(restaurantList);
   const [searchTxt, setSearchTxt] = useState("");
 
+  useEffect(() => {
+    console.log("render useEffect")
+  }, [])
+
+  console.log("reder")
+
+  /*
+    useEffect :-
+        -> if didn't pass [], then it call every time when page re-render
+        -> [] called once
+        -> [restaurants] , restaurants is a dependency and it useEffect calls when the restaurants chnges
+        -> useEffect call after the page load
+  */
+
   return (
     <>
+
+      {/* search component start here  */}
       <div className='search-container'>
         <input
           type="text"
@@ -36,13 +52,17 @@ function Body() {
           setRestaurants(data);
         }}>Search</button>
       </div>
+      {/* search component ends here  */}
 
 
+      {/* main/body component start here  */}
       <div className='main'>
         {restaurants.map((restaurant) => (
           <Card {...restaurant.data} key={restaurant.data.id} />
         ))}
       </div>
+      {/* main/body component ends here  */}
+
     </>
   );
 }
