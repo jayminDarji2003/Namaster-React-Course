e1dbc9ed08344a985b599a0ccd5d848f1615a7c5
Jaymin Darji <jaymindarji2003@gmail.com>
2023-08-28 12:27:09 +0530

    work in progress

--- FoodVilla Application/src/components/Header.js
+++ FoodVilla Application/src/components/Header.js
@@ -1,11 +1,6 @@
 import React, { useState } from 'react'
 import logo from '../Assets/Images/logo.png'
 import { Link } from 'react-router-dom';
-import App from '../App';
-import Cart from './Cart';
-import Profile from './Profile';
-import Authentication from './Authentication';
-
 
 function Header() {
     const [isLogedIn, setIsLogedIn] = useState(true);
@@ -21,28 +16,37 @@ function Header() {
             <div className="navItems">
                 <ul>
                     <li>
-                        <Link to="/">
+                        <Link to="/" className='linkContainer'>
+                            <p>HOME</p>
                             <i className="fa-solid fa-house home"></i>
                         </Link>
                     </li>
 
                     <li>
-                        <Link to="/cart">
+                        <Link to="/cart" className='linkContainer'>
+                            <p> CART</p>
                             <i className="fa-solid fa-cart-shopping cartIcon"></i>
                         </Link>
                     </li>
 
                     <li>
-                        <Link to="/profile">
+                        <Link to="/profile" className='linkContainer'>
+                            <p>PROFILE</p>
                             <i className="fa-solid fa-user profileIcon" ></i>
                         </Link>
                     </li>
 
                     <li>
-                        <Link to="/Authentication">
+                        <Link to="/Authentication" className='linkContainer'>
+
+                            {
+                                (isLogedIn) ? <p>Log Out</p> : <p>Log Out</p>
+                            }
+
                             {
                                 (isLogedIn) ?
-                                    <i className="fa-solid fa-share-from-square authenticationIcon"></i> :
+                                    <i className="fa-solid fa-share-from-square authenticationIcon"></i>
+                                    :
                                     <i className="fa-solid fa-reply-all authenticationIcon"></i>
                             }
                         </Link>
