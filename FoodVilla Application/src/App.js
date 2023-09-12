1ebc39a7e794e5d41589e48dedf52c332eb63b2d
Jaymin Darji <jaymindarji2003@gmail.com>
2023-09-12 09:05:15 +0530

    tailwind css added to my project

--- FoodVilla Application/src/App.js
+++ FoodVilla Application/src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import Body from './components/Body';
+// import Body from './components/Body';
 import Header from './components/Header';
 // import Footer from './components/FooterClass';
 
