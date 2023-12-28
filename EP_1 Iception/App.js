49468f3b01f7aa37fe99a4e9f366efdac699d850
Jaymin Darji <jaymindarji2003@gmail.com>
2023-12-28 18:21:11 +0530

    Day 41 (Ep3_ignite our app done)

--- EP_1 Iception/App.js
+++ EP_1 Iception/App.js
@@ -1,22 +1,16 @@
-// <!--  create element using JAVASCRIPT -->
-//     <!-- <Script> -->
-//     <!-- const heading = document.createElement("h1"); -->
-//     <!-- heading.innerHTML = "Namaste React!!!"; -->
-//     <!-- const root = document.getElementById("root"); -->
-//     <!-- root.appendChild(heading); -->
-//     <!-- </Script> -->
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//     <!-- React CDN links  -->
-
-
-// <!-- Create element using React  -->
 const heading2 = React.createElement("h2", {
+    key: "12",
     id: "heading2"
 }, "Heading 2");
 const heading3 = React.createElement("h3", {
+    key: "13",
     id: "heading3"
 }, "Heading 3");
 const heading4 = React.createElement("h4", {
+    key: "14",
     id: "heading4"
 }, "Heading 4");
 
@@ -24,6 +18,5 @@ const heading = React.createElement("div", {
     id: "headingContainer"
 }, [heading2, heading3, heading4]);
 
-console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
