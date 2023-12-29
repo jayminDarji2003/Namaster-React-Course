c0e9af22cf658d69e025735382937d17dada6692
Jaymin Darji <jaymindarji2003@gmail.com>
2023-12-29 15:19:16 +0530

    Day 42 done

--- Food ordering app/Foodvillah/src/App.jsx
+++ Food ordering app/Foodvillah/src/App.jsx
@@ -1,9 +1,29 @@
 import './App.css'
 
+function Header() {
+  return (
+    <h1>Header component</h1>
+  )
+}
+function Main() {
+  return (
+    <h1>Main component</h1>
+  )
+}
+function Footer() {
+  return (
+    <h1>Footer component</h1>
+  )
+}
+
+
 function App() {
   return (
     <>
       <h1>Welcome to foodvilla application</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
