951eba8cd6edd57edd2bf31034803dc533312ffc
Jaymin Darji <jaymindarji2003@gmail.com>
2024-01-04 17:52:16 +0530

    Day 48

---
+++ Food ordering app/Swiggy_clon/src/Components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+    return (
+        <div className='flex justify-center items-center'>
+            Page not found!!
+        </div>
+    )
+}
+
+export default Error
