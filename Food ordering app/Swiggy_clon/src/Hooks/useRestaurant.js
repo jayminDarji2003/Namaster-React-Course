517844fff0c6a9275ff9ea66791c9a7de9ed4285
Jaymin Darji <jaymindarji2003@gmail.com>
2024-02-05 19:45:24 +0530

    learning how to test our react application

--- Food ordering app/Swiggy_clon/src/Hooks/useRestaurant.js
+++ Food ordering app/Swiggy_clon/src/Hooks/useRestaurant.js
@@ -9,16 +9,23 @@ const useRestaurant = () => {
   }, []);
 
   async function getRestaurant() {
-    const data = await fetch(SWIGGY_D_API_2); // fetching the data from the swiggy api
-    const resData = await data.json(); // converting the data to json format
-    // console.log(resData);
-    setRestaurant(resData); // updating the restaurant
+    let apiToCall = SWIGGY_D_API_2; // Default to desktop API
+
+    // Check if the user is on a mobile device
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+      apiToCall = SWIGGY_M_API; // Use mobile API
+    }
+
+    try {
+      const data = await fetch(apiToCall);
+      const resData = await data.json();
+      setRestaurant(resData);
+    } catch (error) {
+      console.error("Error fetching restaurant data:", error);
+    }
   }
 
   return restaurant;
 };
 
 export default useRestaurant;
-
-
-
--- Food ordering app/Swiggy_clon/src/Hooks/useRestaurantMenu.js
+++ Food ordering app/Swiggy_clon/src/Hooks/useRestaurantMenu.js
@@ -1,3 +1,35 @@
+// import { useEffect, useState } from "react";
+// import { RESTAURANT_DETAILS_D_API, RESTAURANT_DETAILS_M_API } from "../config";
+
+// const useRestaurantMenu = (id) => {
+//   const [restaurant, setRestaurant] = useState(null);
+
+//   useEffect(() => {
+//     getRestaurantInfo();
+//   }, []);
+
+//   async function getRestaurantInfo() {
+//     try {
+//       const data = await fetch(
+//         RESTAURANT_DETAILS_D_API +
+//         id +
+//         "&catalog_qa=undefined&submitAction=ENTER"
+//       );
+//       const json = await data.json();
+//       setRestaurant(json.data);
+//     } catch (error) {
+//       console.error("Error fetching restaurant info:", error);
+//     }
+//   }
+
+//   return restaurant;
+// };
+
+// export default useRestaurantMenu;
+
+
+
+
 import { useEffect, useState } from "react";
 import { RESTAURANT_DETAILS_D_API, RESTAURANT_DETAILS_M_API } from "../config";
 
@@ -9,11 +41,16 @@ const useRestaurantMenu = (id) => {
   }, []);
 
   async function getRestaurantInfo() {
+    let apiToCall = RESTAURANT_DETAILS_D_API; // Default to desktop API
+
+    // Check if the user is on a mobile device
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+      apiToCall = RESTAURANT_DETAILS_M_API; // Use mobile API
+    }
+
     try {
       const data = await fetch(
-        RESTAURANT_DETAILS_D_API +
-        id +
-        "&catalog_qa=undefined&submitAction=ENTER"
+        apiToCall + id + "&catalog_qa=undefined&submitAction=ENTER"
       );
       const json = await data.json();
       setRestaurant(json.data);
@@ -26,4 +63,3 @@ const useRestaurantMenu = (id) => {
 };
 
 export default useRestaurantMenu;
-
